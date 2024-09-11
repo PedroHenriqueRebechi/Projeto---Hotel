9d64c8cbe443485681c37612e94cb8ad60638a43
Pedro Henrique Rebechi <pedrohenriquerebechi@gmail.com>
2024-09-10 22:23:42 -0300

    Estrutura inicial

---
+++ app/module.py
@@ -0,0 +1,12 @@
+# def cadastrar_cliente(nome, cpf, data_nascimento):
+
+
+class Cliente():
+    def __init__(self, nome, cpf, data_nascimento):
+        self.__nome = nome
+        self.__cpf = cpf
+        self.__data_nascimento = data_nascimento
+
+        
+
+    
