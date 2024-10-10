1bbda1e15a11dc14a02690c6bdeb38fe8cd214bc
Pedro Henrique Rebechi <pedrohenriquerebechi@gmail.com>
2024-10-10 20:15:00 -0300

    Pagamentos

--- app/module.py
+++ app/module.py
@@ -23,10 +23,9 @@ class Cliente():
         return f"Cliente: {self.nome} | CPF: ***{self.cpf[4:11]}** | Data de Nascimento: {self.data_nascimento}" # ***.{str(self.cpf)[4:11]}-**
 
 class Quarto():
-    def __init__(self, numero,disponibilidade, preco=100):
+    def __init__(self, numero,disponibilidade):
         self.__disponibilidade = disponibilidade
         self.numero = numero
-        self.preco = preco
     
     @property
     def disponibilidade(self):
@@ -43,15 +42,18 @@ class Quarto():
             return f"Quarto {self.numero}: Indisponível" # até {data_saida}"
 
 class Reserva(Quarto):
-    def __init__(self, quarto, data_entrada, data_saida):
+    def __init__(self, quarto, data_entrada, data_saida, cpf, dias_estadia, preco=100):
         self.quarto = quarto
         self.data_entrada = data_entrada
         self.data_saida = data_saida
+        self.cpf = cpf
+        self.dias_estadia = dias_estadia
+        self.preco = preco
 
-    def reservar(self, quarto, clientes):
-        cpf = str(input("Informe seu CPF para completar a reserva: "))
+    def reservar(self, quarto, clientes, cpf):
+        #cpf = str(cpf)
 
-        cpf = f"{cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:]}"
+        #cpf = f"{cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:]}"
 
         if len(clientes) == 0:
             return print(f"\nO seu CPF não está cadastrado no nosso sistema")
@@ -96,13 +98,13 @@ def listar_clientes(lista_clientes):
     for cliente in lista_clientes:
         print(cliente.__repr__())
         
-
 def menu():
     opcoes = """
 ----MENU----
 [1] Fazer cadastro
 [2] Listar clientes
 [3] Fazer reserva
+[4] Pagar hospedagem
 
 [9] Sair
 
@@ -114,16 +116,21 @@ def escolher_quarto(lista_quartos, lista_clientes, lista_reservas):
     i = 0
     for quarto in lista_quartos:
         print(f"{lista_quartos[i].mostrar_disponibilidade()}", end=" ")
+        i += 1
         for reserva in lista_reservas:
             if reserva.quarto == quarto:
                 print(f"até {reserva.data_saida}")
-        i+=1
+
         print('\n')
     print('------------------')
 
-    quarto_escolhido = str(input("Digite o número do quarto que deseja se hospedar: "))
+    quarto_escolhido = str(input("Digite o número do quarto que deseja se hospedar (digite [0] para voltar): "))
+    if quarto_escolhido == "0":
+        return
+    
     data_entrada = datetime.strptime(str(input("Digite a data do check-in (DD/MM/AAAA): ")), '%d/%m/%Y').date()
     data_saida = datetime.strptime(str(input("Digite a data do check-out (DD/MM/AAAA): ")), '%d/%m/%Y').date()
+    dias_estadia = (data_saida - data_entrada).days
     if data_entrada > data_saida:
         return print(f"\nErro! Coloque datas válidas")
     data_entrada = data_entrada.strftime('%d/%m/%Y')
@@ -131,12 +138,34 @@ def escolher_quarto(lista_quartos, lista_clientes, lista_reservas):
 
     for quarto in lista_quartos:
         if quarto_escolhido == quarto.numero and quarto.disponibilidade == True:
-            nova_reserva = Reserva(quarto, data_entrada, data_saida)
+            cpf = str(input("Digite seu CPF para confirmar (apenas numeros): "))
+            cpf = f"{cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:]}"
+            nova_reserva = Reserva(quarto, data_entrada, data_saida, cpf, dias_estadia)
             lista_reservas.append(nova_reserva)
-            return nova_reserva.reservar(quarto, lista_clientes)
+            return nova_reserva.reservar(quarto, lista_clientes, cpf)
             # return quarto.fazer_reserva(quarto, lista_clientes)
     
     return print("\nQuarto indisponível!")
+
+def soma_diaria(lista_clientes, lista_reservas):
+    cpf = str(input("Digite seu CPF (apenas numeros): "))
+    cpf = f"{cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:]}"
+
+    """if len(lista_clientes) == 0:
+            return print(f"\nO seu CPF não está cadastrado no nosso sistema")"""
+        
+    for cliente in lista_clientes:
+        if cliente.cpf != cpf:
+            return print(f'O seu CPF não está cadastrado no nosso sistema')
+
+    for reserva in lista_reservas:
+        if reserva.cpf == cpf:
+            total = reserva.dias_estadia * reserva.preco
+            return print(f"Você está devendo R${total},00")
+        
+        elif reserva.cpf != cpf:
+            return print(f'Você não tem nenhuma reserva')
+
 
 def main(): 
     clientes = [] # Alterar para banco de dados
@@ -154,6 +183,9 @@ def main():
 
         if opcao == 3:
             escolher_quarto(quartos, clientes, reservas)
+
+        if opcao == 4:
+            soma_diaria(clientes, reservas)
                     
         
         elif opcao == 9: 
